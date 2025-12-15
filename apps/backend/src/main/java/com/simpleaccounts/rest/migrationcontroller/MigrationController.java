bb09010f94a25ffbd5bdc00ac9528534c3a44ff5
MohsinHashmi-DataInn <mohsin@datainn.io>
2025-12-15 00:14:43 -0500

    fix(backend): use commons-lang3 imports

--- apps/backend/src/main/java/com/simpleaccounts/rest/migrationcontroller/MigrationController.java
+++ apps/backend/src/main/java/com/simpleaccounts/rest/migrationcontroller/MigrationController.java
@@ -42,7 +42,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.FileUtils;
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
